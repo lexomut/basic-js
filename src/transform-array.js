2bc9b021f232c9d3fcbd9aa54b1f5e45ac0386ca
lexomut <lexomut@mail.ru>
2020-10-05 20:18:56 +0300

    float: ready transform arrey

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,16 +2,18 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr ) {
   // if (!(Array.isArray(arr)) )return "Error"
+
   let newArr = []
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == '--discard-next') i += 2
+    if (arr[i] == '--discard-next') i ++
     else if (arr[i] == '--discard-prev') {if(arr[i-2]!=='--discard-next') newArr.pop()}
     else if (arr[i] == '--double-next'){ if (i+1<arr.length){
       newArr.push(arr[i + 1]);
       newArr.push(arr[i + 1]);
       i++; }}
-    else if (arr[i] == '--double-prev') if (arr[i-2]=='--discard-next'||!(i>0)){} else newArr.push(arr[i - 1])
+    else if (arr[i] == '--double-prev') {if (arr[i-2]=='--discard-next'||!(i>0)){} else newArr.push(arr[i - 1])}
     else newArr.push(arr[i])
   }
+
   return newArr
 };
