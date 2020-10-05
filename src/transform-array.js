c08786267c2dd4224d919acacd599c5ce65311e8
lexomut <lexomut@mail.ru>
2020-10-05 18:49:51 +0300

    2

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr ) {
+  // if (!(Array.isArray(arr)) )return "Error"
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--discard-next') i += 2
+    else if (arr[i] == '--discard-prev') {if(arr[i-2]!=='--discard-next') newArr.pop()}
+    else if (arr[i] == '--double-next'){ if (i+1<arr.length){
+      newArr.push(arr[i + 1]);
+      newArr.push(arr[i + 1]);
+      i++; }}
+    else if (arr[i] == '--double-prev') if (arr[i-2]=='--discard-next'||!(i>0)){} else newArr.push(arr[i - 1])
+    else newArr.push(arr[i])
+  }
+  return newArr
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date)return 'Unable to determine the time of year!'
+// date+=''
+   // if( "object"!==typeof(date)) return 'Error'
+//
+ if (!date.valueOf())return 'Error'
+//
+//   let date1 = new Date(Date.parse(date))
+  let month=date.getMonth()
+  if (month<5&&month>1) return 'spring'
+     else if (month<8&&month>4) return 'summer'
+      else if (month<11&&month>7) return 'autumn'
+      else return 'winter'
+
+
+
+
+
+
+
 };
