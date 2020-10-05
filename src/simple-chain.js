ccd484429c2e390c66c18d1ced77f4e354655cb4
lexomut <lexomut@mail.ru>
2020-10-06 01:12:30 +0300

    float: ready chinMarker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '') {
+    this.chain.push(`( ${value} )`)
+    return this
+
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chain[position - 1] && Number.isInteger(position)) {
+      this.chain.splice(position - 1, 1);
+      console.log(Number.isInteger(position))
+    } else {
+      this.chain = [];
+      console.log('сброшено')
+      throw new Error('fail position');
+    }
+    return this
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.chain.join('~~');
+
+    this.chain = [];
+    return result;
+
+
   }
-};
+}
 
 module.exports = chainMaker;
