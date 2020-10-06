c0adb5aadee4f13a1be62556fc2530d32c091c7d
lexomut <lexomut@mail.ru>
2020-10-06 21:40:17 +0300

    fleat:  ready

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -2,13 +2,57 @@ const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
   encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!message ||!key) throw new Error('')
+    message=message.toUpperCase()
+    key=key.toUpperCase()
+
+    let result=''
+    let j=0
+
+    for (let i=0; i<message.length;i++){
+      if(/[A-Z]/.test(message[i])) {
+        if(j == key.length) j = 0;
+        let numMessage=message[i].charCodeAt()-64
+        let numKey=key[j].charCodeAt()-64
+        // console.log(numKey,numMessage)
+        let resCode = numMessage + numKey;
+        if (resCode > 26) resCode -=26;
+        resCode +=63
+        //console.log(resCode)
+        result += String.fromCharCode(resCode);
+        j++
+      }
+      else  result += message[i];
+    }
+    return result
+
   }    
   decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!message ||!key) throw new Error('')
+    message=message.toUpperCase()
+    key=key.toUpperCase()
+
+    let result=''
+    let j=0
+
+    for (let i=0; i<message.length;i++){
+      if(/[A-Z]/.test(message[i])) {
+        if(j == key.length) j = 0;
+        let numMessage=message[i].charCodeAt()-64
+        let numKey=key[j].charCodeAt()-64
+        //  console.log(numKey,numMessage)
+        let resCode = numMessage - numKey;
+        if (resCode < 0) resCode +=26;
+        resCode +=65
+        //  console.log(resCode)
+        result += String.fromCharCode(resCode);
+        j++
+      }
+      else  result += message[i];
+    }
+    return result
   }
 }
 
 module.exports = VigenereCipheringMachine;
+// return this.isReverse ? result.split('').reverse().join('') : result;
