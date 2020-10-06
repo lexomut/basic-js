ac8c3e5955e0f8b9e24f81c297057f3e1c66aa64
lexomut <lexomut@mail.ru>
2020-10-06 16:15:39 +0300

    fleat: extended-repeater ready

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options ) {
+    function f(str='',separator='+',repeatTimes=1,addition='',additionRepeatTimes=1,additionSeparator='|') {
+        repeatTimes = +repeatTimes
+        separator=separator+''
+        addition=addition+''
+        additionRepeatTimes=+additionRepeatTimes
+        additionSeparator=additionSeparator+''
+
+        let result=''
+        for (let i=0;i<repeatTimes;i++){
+            result+=str;
+            for (let j=1;j<=additionRepeatTimes;j++){
+                result+=addition
+                if(j!==additionRepeatTimes)    result+=additionSeparator
+            }
+            if(i<repeatTimes-1)result+=separator
+        }
+
+        return result
+    }
+    return f(str,options.separator,options.repeatTimes,options.addition,options.additionRepeatTimes,options.additionSeparator)
 };
-  
